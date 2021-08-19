05e5c1278d32ae651f11df0e1ae694a52048f14a
Destiny Erhabor <destinterhabor6@gmail.com>
2021-08-19 03:48:32 +0100

    post routes

---
+++ routes/resume-route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { ResumeContact } from "../model/resumeContact.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+export const router = express.Router()
+
+router.route('/')
+.post(catchAsync(async(req,res, next)=>{
+  const {name, email, message} = req.body
+  console.log(req.body);
+  try {
+    const resumeContact = await ResumeContact.create({
+      name,
+      email,
+      message
+    })
+    req.flash("success", "Successfully, you will be contacted soon");
+    res.redirect('/')
+    console.log(resumeContact)
+  } catch (error) {
+    next(error)
+    req.flash("error", "An error, kindly refresh your browser and retry");
+    console.log(error);
+  }
+}))
